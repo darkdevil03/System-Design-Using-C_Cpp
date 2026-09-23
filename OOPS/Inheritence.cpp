e062dc3bc049c903ae35ef4f042ffd1aada26abf
darkdevil03 <bvvsatyanarayana280301@gmail.com>
2026-09-23 21:52:34 +0530

    Implemented simple Inheritence.cpp program.

---
+++ OOPS/Inheritence.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <ostream>
+#include <string>
+
+using namespace std;
+
+class Car {
+public:
+    string brand;
+    string model;
+    float engine_cc;
+    int speed;
+
+    Car(){
+        brand = "unknown";
+        model = "unknown";
+        engine_cc = 0.0;
+        speed = 0;
+    }
+
+    void engine_start() {
+        cout << "Car Engine Started!!" << endl;
+    }
+
+    void engine_stop() {
+        cout << "Car Engine Stopped!!" << endl;
+    }
+
+    void accelerate() {
+        speed += 10;
+        cout << "Car Accelerate at speed " << speed << endl;
+    }
+
+    void decelerate() {
+        if (speed == 0) {
+            cout << "Car Decelerate at speed " << speed << endl;
+        }else {
+            speed -= 10;
+            cout << "Car Decelerate at speed " << speed << endl;
+        }
+    }
+
+    void apply_break(){
+        speed = 0;
+        cout << "Car Break at speed " << speed << endl;
+    }
+
+};
+
+int main() {
+
+    Car* c = new Car();
+
+    c->engine_start();
+    c->accelerate();
+    c->decelerate();
+    c->apply_break();
+    c->engine_stop();
+
+
+    delete c;
+
+    return EXIT_SUCCESS;
+}
